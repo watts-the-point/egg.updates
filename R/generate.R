3038c6e94e2eff313ff19d647577fbc45a6a2356
Alex Watts <97633202+watts-the-point@users.noreply.github.com>
2022-12-08 17:37:23 -0500

    Built package. Need to fix some bugs though.

---
+++ R/generate.R
@@ -0,0 +1,16 @@
+#' Generates an EGG update file
+#'
+#' This function prompts the user to provide information about what they
+#' did this week for the EGG group in Pitt's Urology Department.
+#'
+#' @keywords update
+#' @export
+
+
+create_EGG_update <- function(){
+  filename <- paste0("~/Documents/EGG/Weekly_Updates/",Sys.Date(), "_Update.docx")
+
+  rmarkdown::render(
+    "~/R_packages/egg.updates/inst/rmarkdown/templates/weekly-update/skeleton/skeleton.Rmd",
+                  output_file = filename, params = "ask")
+}
